463ecde7a621c62837dc000e40a44115df8e9d71
Shivani Puttaraju <97019847+Shivaniputtaraju@users.noreply.github.com>
2022-02-14 19:27:57 +0000

    p2final.c

--- p2final.c
+++ p2final.c
@@ -4,15 +4,15 @@ int input()
   int a;
   printf("enter the number\n");
   scanf("%d",&a);
-  return 0;
+  return a;
 }
 int cmp(int a,int b,int c)
 {
-  if(a>b&&b>a)
+  if((a>b)&&(a>c))
   return a;
-  else if(b>a&&b>c)
+  else if((b>a)&&(b>c))
   return b;
-  else
+  else((c>a)&&(c>b));
   return c;
 }
 void output(int a,int b,int c,int largest)
@@ -26,6 +26,6 @@ int main()
   b=input();
   c=input();
   largest=cmp(a,b,c);
-  output(a,b,c,&largest);
+  output(a,b,c,largest);
   return 0;
 }
